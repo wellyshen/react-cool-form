a718499e57ec388b0a5f4922994ad83c775680cc
Welly Shen <hivoid19@gmail.com>
2020-09-14 22:59:09 +0800

    try something...

--- examples/src/BasicForm/index.tsx
+++ examples/src/BasicForm/index.tsx
@@ -1,92 +1,47 @@
 /** @jsx jsx */
 
 import { jsx } from "@emotion/core";
-import React, { useState, memo, useCallback, useMemo, useReducer } from "react";
+import React, { memo } from "react";
 import useForm from "react-cool-form";
 
 import { container, form } from "./styles";
 
 const TextField = memo(
-  ({ label, id, ...rest }: any): JSX.Element => {
-    console.log(`${id} is re-rendered`);
+  ({ label, name, ...rest }: any): JSX.Element => {
+    console.log(`${name} is re-rendered`);
 
     return (
       <React.Fragment>
-        <label htmlFor={id}>{label}</label>
-        <input id={id} {...rest} />
+        <label htmlFor={name}>{label}</label>
+        <input id={name} {...rest} />
       </React.Fragment>
     );
   }
 );
 
-const reducer = (state: any, action: any) => {
-  switch (action.type) {
-    case "firstName":
-      return { ...state, firstName: action.payload };
-    case "lastName":
-      return { ...state, lastName: action.payload };
-    default:
-      throw new Error("Unexpected action");
-  }
-};
-
 export default (): JSX.Element => {
-  // @ts-expect-error
-  const res = useForm();
-  console.log("LOG ===> ", res);
-  const [state, dispatch] = useReducer(reducer, {
-    firstName: "",
-    lastName: "",
+  // @ts-ignore
+  const { getInputProps } = useForm({
+    defaultValues: { firstName: "", lastName: "" },
   });
-  // const [values, setValues] = useState<any>({ firstName: "", lastName: "" });
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: any): void => {
     e.preventDefault();
-    console.log("LOG ===> ", state);
+    console.log("LOG ===> ", e);
   };
-
-  const handleChange = useCallback((e: any) => {
-    const { id, value } = e.target;
-    dispatch({ type: id, payload: value });
-    // const { id, value } = e.target;
-    // setValues({ ...values, [id]: value });
-  }, []);
-
-  /* const handleFirstNameChange = useCallback((e: any) => {
-    setFirstName(e.target.value);
-  }, []);
-
-  const handleLastNameChange = useCallback((e: any) => {
-    setLastName(e.target.value);
-  }, []); */
-
-  // const memoValues = useMemo(() => values, [values]);
 
   return (
     <div css={container}>
       <form css={form} onSubmit={handleSubmit} noValidate>
         <TextField
           label="First Name: "
-          id="firstName"
-          name="firstName"
           type="text"
-          required
-          value={state.firstName}
-          onLoad={(e: any) => {
-            console.log("LOG ===> ", e);
-          }}
-          onChange={handleChange}
+          {...getInputProps("firstName")}
         />
         <TextField
           label="Last Name: "
-          id="lastName"
-          name="lastName"
           type="text"
-          required
-          value={state.lastName}
-          onChange={handleChange}
+          {...getInputProps("lastName")}
         />
         <button type="submit">Submit</button>
       </form>
