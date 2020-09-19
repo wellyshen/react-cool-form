1c78fe32bb31a04862b039cd75e1a846e550fd15
Welly Shen <hivoid19@gmail.com>
2020-09-20 02:07:34 +0800

    try something...

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,27 @@ import {
 } from "./types";
 import useFormReducer from "./useFormReducer";
 
-const throwFieldNameWarn = () => {
+const warnNoFieldName = () => {
   if (__DEV__)
     console.warn('💡react-cool-form: Field is missing "name" attribute');
 };
+
+/* const getCheckboxValue = (
+  el: HTMLInputElement,
+  currentValue: any
+): boolean | string[] => {
+  if (!currentValue) {
+    // ...
+  } else {
+    // ...
+  }
+}; */
 
 const useForm = <T extends FieldValues = FieldValues>({
   defaultValues = {},
 }: Options = {}): Return<T> => {
   const formRef = useRef<HTMLFormElement | null>(null);
+  const valuesRef = useRef(defaultValues);
   const [state, dispatch] = useFormReducer<T>(defaultValues);
 
   const setValues = useCallback<SetValues>(
@@ -33,27 +45,56 @@ const useForm = <T extends FieldValues = FieldValues>({
     [dispatch]
   );
 
+  const setDefaultValues = useCallback(() => {
+    if (!formRef.current) return;
+
+    Array.from(formRef.current.children).forEach((child) => {
+      if (!/INPUT|SELECT|TEXTAREA/.test(child.tagName)) return;
+
+      // @ts-expect-error
+      const { type, name, value, checked } = child as InputElements;
+
+      if (!name) {
+        warnNoFieldName();
+        return;
+      }
+      if (type === "file") return;
+
+      const val = defaultValues[name] || checked || value;
+
+      if (type === "checkbox") {
+        // TODO: handle checkbox value
+      }
+
+      setValues(name, val || "");
+    });
+  }, [defaultValues, setValues]);
+
+  useEffect(() => {
+    valuesRef.current = state.values;
+  }, [state.values]);
+
+  useEffect(() => {
+    setDefaultValues();
+  }, [setDefaultValues]);
+
   useEffect(() => {
     if (!formRef.current) return () => null;
 
     const form = formRef.current;
 
-    Array.from(form.children).forEach((child) => {
-      if (!/INPUT|SELECT|TEXTAREA/.test(child.tagName)) return;
-      if (!child.hasAttribute("name")) throwFieldNameWarn();
-    });
-
     const handleChange = (e: Event) => {
-      const target = e.target as InputElements;
-
-      if (!target.name) throwFieldNameWarn();
-
       // @ts-expect-error
-      const { type, name, value, checked } = target;
+      const { name, type, value, checked } = e.target as InputElements;
+
+      if (!name) {
+        warnNoFieldName();
+        return;
+      }
 
       switch (type) {
         case "checkbox":
-          setValues(name, target.hasAttribute("value") ? value : checked);
+          // TODO: handle checkbox value
           break;
         default:
           setValues(name, value);
