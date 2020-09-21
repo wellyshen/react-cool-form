4d74d8f447bb12ff6bf4a4107d9169cfae74bbbe
Welly Shen <hivoid19@gmail.com>
2020-09-22 00:21:35 +0800

    refactor: re-parse form input elements for setDefaultValues() & re-use Fields type & add warning for setting formRef

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { useRef, useCallback, useEffect } from "react";
 
 import {
-  FormActionType,
-  FieldValues,
   Options,
   Return,
+  FormActionType,
+  Fields,
+  FieldValues,
   FieldElements,
   SetValues,
 } from "./types";
@@ -15,33 +16,35 @@ const warnNoFieldName = () => {
     console.warn('💡react-cool-form: Field is missing "name" attribute');
 };
 
-const getInputs = (form: HTMLFormElement) =>
-  Array.from(form.querySelectorAll("input,textarea,select"))
-    .filter((element) => {
-      const field = element as FieldElements;
+const getFields = (form: HTMLFormElement | null) =>
+  form
+    ? Array.from(form.querySelectorAll("input,textarea,select"))
+        .filter((element) => {
+          const field = element as FieldElements;
 
-      if (!field.name) {
-        warnNoFieldName();
-        return false;
-      }
-      if (
-        !/TEXTAREA|SELECT/.test(field.tagName) &&
-        /hidden|image|file|submit|reset/.test(field.type)
-      )
-        return false;
+          if (!field.name) {
+            warnNoFieldName();
+            return false;
+          }
+          if (
+            !/TEXTAREA|SELECT/.test(field.tagName) &&
+            /hidden|image|file|submit|reset/.test(field.type)
+          )
+            return false;
 
-      return true;
-    })
-    .reduce((acc, cur) => {
-      acc[(cur as FieldElements).name] = cur;
-      return acc;
-    }, {} as Record<string, any>);
+          return true;
+        })
+        .reduce((acc, cur) => {
+          acc[(cur as FieldElements).name] = cur;
+          return acc;
+        }, {} as Record<string, any>)
+    : {};
 
 const useForm = <T extends FieldValues = FieldValues>({
   defaultValues = {},
 }: Options = {}): Return<T> => {
   const formRef = useRef<HTMLFormElement | null>(null);
-  const fieldsRef = useRef<Record<string, FieldElements>>({});
+  const fieldsRef = useRef<Fields>({});
   const [state, dispatch] = useFormReducer<T>(defaultValues);
 
   const setFieldValue = useCallback((name: string, value: any) => {
@@ -81,23 +84,27 @@ const useForm = <T extends FieldValues = FieldValues>({
     [dispatch]
   );
 
-  const setDefaultValues = useCallback(() => {
-    const { current: fields } = fieldsRef;
+  const setDefaultValues = useCallback(
+    (fields: Fields = getFields(formRef.current)) =>
+      Object.keys(fields).forEach((key) => {
+        const { name } = fields[key];
 
-    if (!fields) return;
-
-    Object.keys(fields).forEach((key) => {
-      const { name } = fields[key];
-
-      setFieldValue(name, defaultValues[name]);
-    });
-  }, [setFieldValue, defaultValues]);
+        setFieldValue(name, defaultValues[name]);
+      }),
+    [setFieldValue, defaultValues]
+  );
 
   useEffect(() => {
-    if (!formRef.current) return;
+    if (!formRef.current) {
+      if (__DEV__)
+        console.warn(
+          '💡react-cool-form: Don\'t forget to register your form with the "formRef"'
+        );
+      return;
+    }
 
-    fieldsRef.current = getInputs(formRef.current);
-    setDefaultValues();
+    fieldsRef.current = getFields(formRef.current);
+    setDefaultValues(fieldsRef.current);
   }, [setDefaultValues]);
 
   useEffect(() => {
