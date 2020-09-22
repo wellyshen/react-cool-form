2549ce5cb1446eec99a11dc25181ded5b45cb71c
Welly Shen <hivoid19@gmail.com>
2020-09-22 12:50:22 +0800

    feat: use utility functions

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+import { FieldElements } from "./types";
+
+export const isCheckbox = (element: FieldElements): boolean =>
+  element.type === "checkbox";
+
+export const isRadio = (element: FieldElements): boolean =>
+  element.type === "radio";
+
+export const isMultipleSelect = (element: FieldElements): boolean =>
+  element.type === "select-multiple";
+
+export const isFile = (element: FieldElements): boolean =>
+  element.type === "file";
+
+export const isObject = (value: unknown): boolean => typeof value === "object";
+
+export const isString = (value: unknown): boolean => typeof value === "string";
