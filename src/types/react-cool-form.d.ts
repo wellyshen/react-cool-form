23a3f9cf40203977e040ce00961b79a5aad472ca
Welly Shen <hivoid19@gmail.com>
2020-09-27 15:38:00 +0800

    feat: add type definition

--- src/types/react-cool-form.d.ts
+++ src/types/react-cool-form.d.ts
@@ -1,3 +1,30 @@
 declare module "react-cool-form" {
-  // TODO: types
+  import { RefObject } from "react";
+
+  export type Values<T> = Partial<T>;
+
+  export type Touched = Record<string, boolean>;
+
+  export type FieldValues = Record<string, any>;
+
+  export interface SetFieldValue<T> {
+    <K extends keyof T>(name: K, value: T[K] | ((value?: T[K]) => T[K])): void;
+  }
+
+  export interface Config<T> {
+    defaultValues: Values<T>;
+  }
+
+  export interface Return<T> {
+    formRef: RefObject<HTMLFormElement>;
+    readonly values: Values<T>;
+    readonly touched: Touched;
+    setFieldValue: SetFieldValue<T>;
+  }
+
+  const useForm: <T extends FieldValues = FieldValues>(
+    config: Config<T>
+  ) => Return<T>;
+
+  export default useForm;
 }
