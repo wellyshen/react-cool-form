705fdbbaf5d81b9ec7b8da4d575ea6dc9b30df17
Welly Shen <hivoid19@gmail.com>
2020-10-21 16:10:03 +0800

    chore: adjust examples

--- examples/src/BasicForm/index.tsx
+++ examples/src/BasicForm/index.tsx
@@ -3,7 +3,7 @@
 
 import { jsx } from "@emotion/core";
 import React, { memo, forwardRef, useState, useEffect } from "react";
-import useForm, { Errors } from "react-cool-form";
+import useForm from "react-cool-form";
 
 import { container, form, label as labelStyle, wrapper } from "./styles";
 
@@ -104,27 +104,26 @@ export default (): JSX.Element => {
     validateForm,
   } = useForm<FormValues>({
     defaultValues,
-    validateOnChange: false,
+    // validateOnChange: false,
     // validateOnBlur: false,
-    // @ts-ignore
-    // validate: async ({ text, hiddenText }, set) => {
-    //   const errors: Errors<{ text: { test: string } }> = { text: { test: "" } };
+    validate: async ({ text, hiddenText }, set) => {
+      const errors = { text: { nest: "" } };
 
-    //   fib(35);
+      fib(35);
 
-    //   // eslint-disable-next-line
-    //   /* await new Promise((resolve) => {
-    //     setTimeout(resolve, 1000);
-    //   }); */
+      // eslint-disable-next-line
+      /* await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      }); */
 
-    //   if (text.nest.length <= 3) set(errors, "text.test", "Form error");
-    //   // if (hiddenText.length <= 3) errors.hiddenText = "Form error";
+      if (text.nest.length <= 3) set(errors, "text.nest", "Form error");
+      // if (hiddenText.length <= 3) errors.hiddenText = "Form error";
 
-    //   // throw new Error("Fake error");
-    //   // return false;
-    //   // return { text: { nest: ["Form error"] } };
-    //   return errors;
-    // },
+      // throw new Error("Fake error");
+      // return false;
+      // return { text: { nest: ["Form error"] } };
+      return errors;
+    },
   });
 
   console.log("LOG ===> formState: ", formState.errors);
