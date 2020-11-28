fa7ebf6d56f145a82cb30f1e981fc6b3b00f9df6
Welly Shen <hivoid19@gmail.com>
2020-11-29 00:00:30 +0800

    refactor(utils): delete the filtered property for "filterError()"

---
+++ .changeset/many-bobcats-shout.md
@@ -0,0 +1,5 @@
+---
+"react-cool-form": patch
+---
+
+refactor(utils): delete the filtered property for `filterError()`
--- src/utils/filterError.ts
+++ src/utils/filterError.ts
@@ -2,13 +2,20 @@ import isPlainObject from "./isPlainObject";
 
 const filterError = (error: unknown, touched: unknown): any => {
   if (!isPlainObject(error) || error instanceof Date)
-    return touched ? error : undefined;
+    return touched ? error : false;
 
   return Object.keys(error).reduce((obj: Record<string, any>, key) => {
-    obj[key] = filterError(
+    const nextError = filterError(
       (error as Record<string, any>)[key],
       (touched as Record<string, any>)[key] || false
     );
+
+    if (nextError) {
+      obj[key] = nextError;
+    } else {
+      delete obj[key];
+    }
+
     return obj;
   }, {});
 };
