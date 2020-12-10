9323a1c7880005ff357a82f3ac5c52058e948696
Welly <hivoid19@gmail.com>
2020-12-10 20:40:09 +0800

    chore(examples): update showbox

--- examples/showbox/index.js
+++ examples/showbox/index.js
@@ -8,32 +8,23 @@ let count = 0;
 
 function App() {
   count++;
-  const { form, getState } = useForm({
+  const { form } = useForm({
     defaultValues: { firstName: "", lastName: "", framework: "" },
     onSubmit: (values) => alert(JSON.stringify(values))
   });
-  const errors = getState("errors");
 
   return (
     <form ref={form} noValidate>
       <div className="count">Render {count} times</div>
-      <div>
-        <input name="firstName" placeholder="First name" required />
-        {errors.firstName && <p>{errors.firstName}</p>}
-      </div>
-      <div>
-        <input name="lastName" placeholder="Last name" required />
-        {errors.lastName && <p>{errors.lastName}</p>}
-      </div>
-      <div>
-        <select name="framework">
-          <option value="">I'm interesting in...</option>
-          <option value="react">React</option>
-          <option value="vue">Vue</option>
-          <option value="angular">Angular</option>
-          <option value="svelte">Svelte</option>
-        </select>
-      </div>
+      <input name="firstName" placeholder="First name" />
+      <input name="lastName" placeholder="Last name" />
+      <select name="framework">
+        <option value="">I'm interesting in...</option>
+        <option value="react">React</option>
+        <option value="vue">Vue</option>
+        <option value="angular">Angular</option>
+        <option value="svelte">Svelte</option>
+      </select>
       <input type="submit" />
     </form>
   );
