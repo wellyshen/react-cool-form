c113f22d05b2adc95d7ae7556d3c41c03e800ad0
Welly <hivoid19@gmail.com>
2020-12-12 17:27:49 +0800

    chore(examples): update form-level validation example

--- examples/form-level-validation/index.js
+++ examples/form-level-validation/index.js
@@ -4,11 +4,28 @@ import { useForm } from "react-cool-form";
 
 import "./styles.scss";
 
+const validate = async ({ name, email }) => {
+  const errors = {};
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  if (!name.length) errors.name = "Required";
+
+  if (!email.length) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
+};
+
 function App() {
   const { form } = useForm({
     defaultValues: { name: "", email: "" },
+    validate,
     onSubmit: (values) => alert(JSON.stringify(values)),
-    onError: (errors) => console.log("onError: ", errors),
+    onError: (errors) => console.log("onError: ", errors)
   });
 
   return (
