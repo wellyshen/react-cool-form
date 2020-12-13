0186ea097d9dd690aa060a310c022c8ec1865b7a
Welly <hivoid19@gmail.com>
2020-12-13 15:43:18 +0800

    chore(examples): update schema-validation

--- examples/schema-validation/index.js
+++ examples/schema-validation/index.js
@@ -4,26 +4,9 @@ import { useForm } from "react-cool-form";
 
 import "./styles.scss";
 
-const validate = async ({ username, email }) => {
-  const errors = {};
-
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  if (!username.length) errors.username = "Required";
-
-  if (!email.length) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
-    errors.email = "Invalid email address";
-  }
-
-  return errors;
-};
-
 function App() {
   const { form } = useForm({
-    defaultValues: { username: "", email: "" },
-    validate,
+    defaultValues: { username: "", email: "", password: "" },
     onSubmit: (values) => alert(JSON.stringify(values)),
     onError: (errors) => console.log("onError: ", errors)
   });
@@ -32,6 +15,7 @@ function App() {
     <form ref={form} noValidate>
       <input name="username" placeholder="Username" />
       <input name="email" type="email" placeholder="Email" />
+      <input name="password" type="password" placeholder="Password" />
       <input type="submit" />
     </form>
   );
