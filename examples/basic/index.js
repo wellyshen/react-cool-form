331aeb7a0dee6f0db799098ca706421bfc68198b
Welly <hivoid19@gmail.com>
2020-12-21 15:07:35 +0800

    chore(examples): update basic example

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -6,15 +6,21 @@ import "./styles.scss";
 
 function App() {
   const { form } = useForm({
-    defaultValues: { firstName: "", lastName: "", email: "" },
+    defaultValues: { username: "", framework: "", message: "" },
     onSubmit: (values) => alert(JSON.stringify(values, undefined, 2))
   });
 
   return (
     <form ref={form} noValidate>
-      <input name="firstName" placeholder="First Name" />
-      <input name="lastName" placeholder="Last Name" />
-      <input name="email" type="email" placeholder="Email" />
+      <input name="username" placeholder="Username" />
+      <select name="framework">
+        <option value="">I'm interesting in...</option>
+        <option value="react">React</option>
+        <option value="vue">Vue</option>
+        <option value="angular">Angular</option>
+        <option value="svelte">Svelte</option>
+      </select>
+      <textarea name="message" placeholder="Leave a message" />
       <input type="submit" />
     </form>
   );
