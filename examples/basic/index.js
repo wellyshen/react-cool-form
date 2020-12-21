21e784fe851d3b4c51e4615f1d0a7fc3bd12b09b
Welly Shen <hivoid19@gmail.com>
2020-12-21 14:34:54 +0800

    chore(examples): add basic example

---
+++ examples/basic/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "react-dom";
+import { useForm } from "react-cool-form";
+
+import "./styles.scss";
+
+function App() {
+  const { form, getState } = useForm({
+    defaultValues: { firstName: "", lastName: "", email: "" },
+    onSubmit: (values) => alert(JSON.stringify(values, undefined, 2)),
+  });
+
+  return (
+    <form ref={form} noValidate>
+      <input name="firstName" placeholder="First Name" />
+      <input name="lastName" placeholder="Last Name" />
+      <input name="email" type="email" placeholder="Email" />
+      <input type="submit" />
+    </form>
+  );
+}
+
+render(<App />, document.getElementById("root"));
