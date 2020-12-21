c48723020fa795b158e368be16dbd81f747fa822
Welly Shen <hivoid19@gmail.com>
2020-12-21 12:47:47 +0800

    feat(useForm): add field-names callback for the option of "setValues"

--- src/utils/isFieldElement.ts
+++ src/utils/isFieldElement.ts
@@ -1,5 +1,4 @@
 import { FieldElement } from "../types";
-import isInputElement from "./isInputElement";
 
 export default (element: HTMLElement): element is FieldElement =>
-  isInputElement(element) || /TEXTAREA|SELECT/.test(element.tagName);
+  /INPUT|TEXTAREA|SELECT/.test(element.tagName);
--- src/utils/setTrueValues.ts
+++ src/utils/setTrueValues.ts
@@ -2,8 +2,8 @@ import { Map } from "../types";
 import set from "./set";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (object: any, paths: string[] | Record<string, any>): Map =>
-  (Array.isArray(paths) ? paths : Object.keys(paths)).reduce((obj, path) => {
-    obj = set(obj, path, true, true);
+export default (object: any, names: string[]): Map =>
+  names.reduce((obj, name) => {
+    obj = set(obj, name, true, true);
     return obj;
   }, object);
