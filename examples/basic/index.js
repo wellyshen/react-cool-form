38495ef8c3816df6b066607377fd15cff1f4e2b8
Welly <hivoid19@gmail.com>
2020-12-22 12:08:47 +0800

    chore(examples): update basic

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -4,6 +4,29 @@ import { useForm } from "react-cool-form";
 
 import "./styles.scss";
 
+const Field = ({ label, id, ...rest }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <input id={id} {...rest} />
+  </div>
+);
+
+const Select = ({ label, id, children, ...rest }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <select id={id} {...rest}>
+      {children}
+    </select>
+  </div>
+);
+
+const Textarea = ({ label, id, ...rest }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <textarea id={id} {...rest} />
+  </div>
+);
+
 function App() {
   const { form } = useForm({
     defaultValues: { firstName: "", lastName: "", framework: "", message: "" },
@@ -12,16 +35,16 @@ function App() {
 
   return (
     <form ref={form} noValidate>
-      <input name="firstName" placeholder="First Name" />
-      <input name="lastName" placeholder="Last Name" />
-      <select name="framework">
+      <Field label="First Name" id="first-name" name="firstName" />
+      <Field label="Last Name" id="last-name" name="lastName" />
+      <Select label="Framework" id="framework" name="framework">
         <option value="">I'm interesting in...</option>
         <option value="react">React</option>
         <option value="vue">Vue</option>
         <option value="angular">Angular</option>
         <option value="svelte">Svelte</option>
-      </select>
-      <textarea name="message" placeholder="Leave a message" />
+      </Select>
+      <Textarea label="Message" id="message" name="message" />
       <input type="submit" />
     </form>
   );
