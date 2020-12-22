b7c92a236e48fc72004fc6b169ba804e5c3f9994
Welly <hivoid19@gmail.com>
2020-12-22 11:34:52 +0800

    chore(examples): update quick start

--- examples/quick-start/index.js
+++ examples/quick-start/index.js
@@ -4,6 +4,14 @@ import { useForm } from "react-cool-form";
 
 import "./styles.scss";
 
+const Field = ({ label, id, error, ...rest }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <input id={id} {...rest} />
+    {error && <p>{error}</p>}
+  </div>
+);
+
 function App() {
   const { form, getState } = useForm({
     defaultValues: { username: "", email: "", password: "" },
@@ -13,24 +21,30 @@ function App() {
 
   return (
     <form ref={form} noValidate>
-      <div>
-        <input name="username" placeholder="Username" required />
-        {errors.username && <p>{errors.username}</p>}
-      </div>
-      <div>
-        <input name="email" type="email" placeholder="Email" required />
-        {errors.email && <p>{errors.email}</p>}
-      </div>
-      <div>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          minLength={6}
-        />
-        {errors.password && <p>{errors.password}</p>}
-      </div>
+      <Field
+        label="Username"
+        id="username"
+        name="username"
+        required
+        error={errors.username}
+      />
+      <Field
+        label="Email"
+        id="email"
+        name="email"
+        type="email"
+        required
+        error={errors.email}
+      />
+      <Field
+        label="Password"
+        id="password"
+        name="password"
+        type="password"
+        required
+        minLength={6}
+        error={errors.password}
+      />
       <input type="submit" />
     </form>
   );
