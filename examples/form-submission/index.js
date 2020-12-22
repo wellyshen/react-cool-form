400107ce9ec0bfce299281d3c305799b125caf7c
Welly Shen <hivoid19@gmail.com>
2020-12-22 16:25:58 +0800

    chore(examples): add submission example

---
+++ examples/form-submission/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render } from "react-dom";
+import { useForm } from "react-cool-form";
+
+import "./styles.scss";
+
+const submitHandler = async (values) => {
+  await new Promise((r) => setTimeout(r, 2000));
+  alert(JSON.stringify(values, undefined, 2));
+};
+
+const errorHandler = (errors) => {
+  console.log("onError: ", errors);
+};
+
+function App() {
+  const { form, getState } = useForm({
+    defaultValues: { username: "", email: "" },
+    validate,
+    onSubmit: submitHandler,
+    onError: errorHandler,
+  });
+  const isSubmitting = getState("isSubmitting");
+
+  return (
+    <form ref={form} noValidate>
+      <input name="username" placeholder="Username" required />
+      <input name="email" type="email" placeholder="Email" required />
+      <input type="submit" disabled={isSubmitting} />
+    </form>
+  );
+}
+
+render(<App />, document.getElementById("root"));
