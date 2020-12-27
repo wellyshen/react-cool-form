3fe57df21f9462d60590adbb34f4ff18509341e5
Welly <hivoid19@gmail.com>
2020-12-27 20:54:02 +0800

    chore(example): update lazy-default-values

--- examples/lazy-default-values/index.js
+++ examples/lazy-default-values/index.js
@@ -11,40 +11,19 @@ const Field = ({ label, id, ...rest }) => (
   </div>
 );
 
-const Select = ({ label, id, children, ...rest }) => (
-  <div>
-    <label htmlFor={id}>{label}</label>
-    <select id={id} {...rest}>
-      {children}
-    </select>
-  </div>
-);
-
-const Textarea = ({ label, id, ...rest }) => (
-  <div>
-    <label htmlFor={id}>{label}</label>
-    <textarea id={id} {...rest} />
-  </div>
-);
-
 function App() {
-  const { form } = useForm({
-    defaultValues: { firstName: "", lastName: "", framework: "", message: "" },
+  const { form, reset } = useForm({
     onSubmit: (values) => alert(JSON.stringify(values, undefined, 2))
   });
+
+  React.useEffect(() => {
+    reset({ firstName: "Welly", lastName: "Shen" });
+  }, [reset]);
 
   return (
     <form ref={form} noValidate>
       <Field label="First Name" id="first-name" name="firstName" />
       <Field label="Last Name" id="last-name" name="lastName" />
-      <Select label="Framework" id="framework" name="framework">
-        <option value="">I'm interesting in...</option>
-        <option value="react">React</option>
-        <option value="vue">Vue</option>
-        <option value="angular">Angular</option>
-        <option value="svelte">Svelte</option>
-      </Select>
-      <Textarea label="Message" id="message" name="message" />
       <input type="submit" />
     </form>
   );
