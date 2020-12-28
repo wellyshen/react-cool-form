2f0ab0eb4cd2a98aec9e23ec09ec438ddef4ba46
Welly Shen <hivoid19@gmail.com>
2020-12-28 19:02:23 +0800

    chore: update playground

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,33 +1,22 @@
-import { useEffect } from "react";
 import { useForm } from "react-cool-form";
 
 interface FormValues {
-  t1?: string;
-  t2: string;
+  t1: string;
 }
 
 const defaultValues = {
-  // t1: "form test",
-  t2: "form test",
+  t1: "form test",
 };
 
 const Playground = (): JSX.Element => {
-  const { form, reset, getState } = useForm<FormValues>();
-  console.log(
-    "LOG ===> ",
-    getState({ isDirty: "isDirty", dirtyFields: "dirtyFields" })
-  );
-
-  useEffect(() => {
-    reset({ t2: "new test" });
-  }, [reset]);
+  const { form } = useForm<FormValues>({
+    onSubmit: (values) => console.log("LOG ===> onSubmit", values),
+  });
 
   return (
     <form ref={form} noValidate>
       <input name="t1" defaultValue="field test" />
-      <input name="t2" />
       <input type="submit" />
-      <input type="reset" />
     </form>
   );
 };
