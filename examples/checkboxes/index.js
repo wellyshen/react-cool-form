77967879816c0b8592cfe878c71da7f318577b15
Welly <hivoid19@gmail.com>
2020-12-28 12:39:55 +0800

    chore(examples): update checkboxes

--- examples/checkboxes/index.js
+++ examples/checkboxes/index.js
@@ -5,25 +5,59 @@ import { useForm } from "react-cool-form";
 import "./styles.scss";
 
 const Field = ({ label, id, ...rest }) => (
-  <div>
-    <label htmlFor={id}>{label}</label>
+  <>
     <input id={id} {...rest} />
+    <label htmlFor={id}>{label}</label>
+  </>
+);
+
+const Group = ({ title, children, error }) => (
+  <div>
+    <fieldset>
+      <legend>{title}</legend>
+      {children}
+    </fieldset>
+    {error && <p>{error}</p>}
   </div>
 );
 
 function App() {
-  const { form, reset } = useForm({
+  const { form, getState } = useForm({
+    defaultValues: { single: true, multiple: [] },
+    validate: (values) => {
+      if (!values.multiple.length) return { multiple: "Required" };
+    },
     onSubmit: (values) => alert(JSON.stringify(values, undefined, 2))
   });
 
-  React.useEffect(() => {
-    reset({ firstName: "Welly", lastName: "Shen" });
-  }, [reset]);
-
   return (
     <form ref={form} noValidate>
-      <Field label="First Name" id="first-name" name="firstName" />
-      <Field label="Last Name" id="last-name" name="lastName" />
+      <Group title="Single">
+        <Field label="RCF" id="single" name="single" type="checkbox" />
+      </Group>
+      <Group title="Multitple" error={getState("errors.multiple")}>
+        <Field
+          label="🍎"
+          id="apple"
+          name="multiple"
+          value="🍎"
+          type="checkbox"
+        />
+        <Field
+          label="🥝"
+          id="kiwi"
+          name="multiple"
+          value="🥝"
+          type="checkbox"
+        />
+        <Field
+          label="🍋"
+          id="lemon"
+          name="multiple"
+          value="🍋"
+          type="checkbox"
+        />
+      </Group>
       <input type="submit" />
     </form>
   );
