9a1728bef866925f4d6805f952688740c9d0f9f9
Welly Shen <hivoid19@gmail.com>
2021-01-02 00:20:07 +0800

    Test: adjust test cases

--- src/utils/__tests__/get.ts
+++ src/utils/__tests__/get.ts
@@ -1,7 +1,7 @@
 import get from "../get";
 
 describe("get", () => {
-  it("should return default value when target isn't an object", () => {
+  it('should return "undefined" when input isn\'t an object', () => {
     expect(get(null, "foo")).toBeUndefined();
     expect(get(undefined, "foo")).toBeUndefined();
     expect(get(true, "foo")).toBeUndefined();
@@ -11,7 +11,7 @@ describe("get", () => {
     expect(get(() => null, "foo")).toBeUndefined();
   });
 
-  it("should return default value when path is invalid", () => {
+  it('should return "undefined" when path is invalid', () => {
     // @ts-ignore
     expect(get({}, null)).toBeUndefined();
     // @ts-ignore
@@ -19,12 +19,36 @@ describe("get", () => {
     expect(get({}, "")).toBeUndefined();
   });
 
+  it("should return default value if property not found", () => {
+    expect(get({}, "foo", "🍎")).toBe("🍎");
+    expect(get({ foo: [] }, "foo.0", "🍎")).toBe("🍎");
+    expect(get({ foo: [] }, "foo[0]", "🍎")).toBe("🍎");
+    expect(get({ foo: [] }, "foo.bar", "🍎")).toBe("🍎");
+    expect(get({ foo: [{}] }, "foo.0.bar", "🍎")).toBe("🍎");
+    expect(get({ foo: [{}] }, "foo[0].bar", "🍎")).toBe("🍎");
+  });
+
   it("should get value by keys", () => {
     expect(get({ foo: "🍎" }, "foo")).toBe("🍎");
-    expect(get({ foo: ["🍎"] }, "foo")).toEqual(["🍎"]);
-    expect(get({ foo: null }, "foo")).toBeNull();
-    expect(get({ foo: undefined }, "foo")).toBeUndefined();
-    expect(get({ foo: true }, "foo")).toBe(true);
-    expect(get({ foo: 1 }, "foo")).toBe(1);
+    expect(get({ foo: { bar: "🍎" } }, "foo.bar")).toBe("🍎");
+    expect(get({ foo: { bar: { baz: "🍎" } } }, "foo.bar.baz")).toBe("🍎");
+  });
+
+  it("should get value by indexes", () => {
+    expect(get({ foo: ["🍎"] }, "foo.0")).toBe("🍎");
+    expect(get({ foo: ["🍋", "🍎"] }, "foo.1")).toBe("🍎");
+    expect(get({ foo: [["🍎"]] }, "foo.0.0")).toBe("🍎");
+    expect(get({ foo: [["🍋", "🍎"]] }, "foo.0.1")).toBe("🍎");
+
+    expect(get({ foo: ["🍎"] }, "foo[0]")).toBe("🍎");
+    expect(get({ foo: ["🍋", "🍎"] }, "foo[1]")).toBe("🍎");
+    expect(get({ foo: [["🍎"]] }, "foo[0][0]")).toBe("🍎");
+    expect(get({ foo: [["🍋", "🍎"]] }, "foo[0][1]")).toBe("🍎");
+  });
+
+  it("should get value by mixed", () => {
+    expect(get({ foo: { bar: [["🍋", "🍎"]] } }, "foo.bar[0].1")).toBe("🍎");
+    expect(get({ foo: [{ bar: ["🍋", "🍎"] }] }, "foo.0.bar[1]")).toBe("🍎");
+    expect(get({ foo: [["🍋", { bar: "🍎" }]] }, "foo[0].1.bar")).toBe("🍎");
   });
 });
