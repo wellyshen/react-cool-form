dde7a7194a9e3f6a81316aa32cb3fb5b9b35c9d7
Welly <hivoid19@gmail.com>
2021-01-01 13:34:39 +0800

    chore(examples): update complex-structures example

--- examples/complex-structures/index.js
+++ examples/complex-structures/index.js
@@ -4,47 +4,53 @@ import { useForm } from "react-cool-form";
 
 import "./styles.scss";
 
-const Field = ({ label, id, ...rest }) => (
-  <div>
-    <label htmlFor={id}>{label}</label>
-    <input id={id} {...rest} />
-  </div>
-);
-
-const Select = ({ label, id, children, ...rest }) => (
-  <div>
-    <label htmlFor={id}>{label}</label>
-    <select id={id} {...rest}>
-      {children}
-    </select>
-  </div>
-);
-
-const Textarea = ({ label, id, ...rest }) => (
-  <div>
-    <label htmlFor={id}>{label}</label>
-    <textarea id={id} {...rest} />
-  </div>
+const FieldGroup = ({ name, onUpdate, onClear, ...rest }) => (
+  <>
+    <input name={name} placeholder={name} />
+    <div>
+      <button type="button" onClick={onUpdate}>
+        Update {name}
+      </button>
+      <button type="button" onClick={onClear}>
+        Clear {name}
+      </button>
+    </div>
+  </>
 );
 
 function App() {
-  const { form } = useForm({
-    defaultValues: { firstName: "", lastName: "", framework: "", message: "" },
+  const { form, setFieldValue } = useForm({
+    defaultValues: {
+      foo: "",
+      bar: [],
+      baz: { a: "" },
+      qux: [{ a: "" }]
+    },
     onSubmit: (values) => alert(JSON.stringify(values, undefined, 2))
   });
 
   return (
     <form ref={form}>
-      <Field label="First Name" id="first-name" name="firstName" />
-      <Field label="Last Name" id="last-name" name="lastName" />
-      <Select label="Framework" id="framework" name="framework">
-        <option value="">I'm interesting in...</option>
-        <option value="react">React</option>
-        <option value="vue">Vue</option>
-        <option value="angular">Angular</option>
-        <option value="svelte">Svelte</option>
-      </Select>
-      <Textarea label="Message" id="message" name="message" />
+      <FieldGroup
+        name="foo"
+        onUpdate={() => setFieldValue("foo", "🍎")}
+        onClear={() => setFieldValue("foo")}
+      />
+      <FieldGroup
+        name="bar[0]"
+        onUpdate={() => setFieldValue("bar[0]", "🍋")}
+        onClear={() => setFieldValue("bar[0]")}
+      />
+      <FieldGroup
+        name="baz.a"
+        onUpdate={() => setFieldValue("baz.a", "🥝")}
+        onClear={() => setFieldValue("baz.a")}
+      />
+      <FieldGroup
+        name="qux[0].a"
+        onUpdate={() => setFieldValue("qux[0].a", "🍉")}
+        onClear={() => setFieldValue("qux[0].a")}
+      />
       <input type="submit" />
     </form>
   );
