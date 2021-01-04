1a7bab4e0741b14a37851413b819e3641624f531
Welly Shen <hivoid19@gmail.com>
2021-01-05 00:55:25 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,68 +1,35 @@
-import { memo, useState } from "react";
 import { useForm } from "react-cool-form";
-import Select from "react-select";
-
-const Controller = memo(
-  ({
-    as,
-    name,
-    defaultValue,
-    parse,
-    format,
-    onChange = () => null,
-    onBlur = () => null,
-    controller,
-    ...rest
-  }: any) => {
-    const Component = as;
-    const [value, setValue] = useState(defaultValue);
-
-    return (
-      <Component
-        {...controller(name, {
-          value,
-          parse,
-          format,
-          onChange: (...args: any[]) => {
-            const fieldValue = args.pop();
-            setValue(fieldValue);
-            onChange(...args);
-          },
-          onBlur,
-        })}
-        {...rest}
-      />
-    );
-  }
-);
-
-const options = [
-  { label: "React", value: "react" },
-  { label: "Vue", value: "vue" },
-  { label: "Angular", value: "angular" },
-  { label: "Svelte", value: "svelte" },
-];
+import { TextField } from "@material-ui/core";
 
 export default () => {
-  const { form, controller } = useForm({
-    defaultValues: { framework: "" },
+  const { form, getState, setFieldValue, validateField } = useForm({
+    defaultValues: { username: "" },
+    validate: ({ username }) => {
+      const errors = {};
+
+      // @ts-expect-error
+      if (!username.length) errors.username = "Required";
+
+      console.log("LOG ===> ", errors);
+
+      return errors;
+    },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
-
-  console.log("LOG ===> Re-render!");
+  const [value, errors] = getState(["values.username", "errors"], {
+    filterUntouchedError: false,
+  });
 
   return (
     <form ref={form}>
-      <Controller
-        as={Select}
-        name="framework"
-        defaultValue=""
-        options={options}
-        parse={(option: any) => option.value}
-        format={(value: any) =>
-          options.find((option) => option.value === value)
-        }
-        controller={controller}
+      <TextField
+        name="username" // Used for the "ignoreFields" option
+        value={value}
+        onChange={(e) => setFieldValue("username", e.target.value)}
+        onBlur={() => validateField("username")}
+        error={!!errors.username}
+        helperText={errors.username}
+        inputProps={{ "data-rcf-ignore": true }} // Ignore the field via the pre-defined data attribute
       />
       <input type="submit" />
     </form>
