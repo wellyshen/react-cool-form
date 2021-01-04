1e4e2cde1dffa8a100830366fb91ad855df7577b
Welly Shen <hivoid19@gmail.com>
2021-01-04 14:36:14 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,61 +1,19 @@
-import { memo, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-cool-form";
-// @ts-expect-error
-import Select from "react-select";
 
-interface FormValues {
-  food: string;
-}
-
-const Optimizer = memo(
-  ({ as, name, defaultValue, parse, format, controller, ...rest }: any) => {
-    const Component = as;
-    const [value, setValue] = useState(defaultValue);
-
-    return (
-      <Component
-        {...controller(name, {
-          value,
-          parse,
-          format,
-          onChange: (e: any, val: any) => setValue(val),
-        })}
-        {...rest}
-      />
-    );
-  }
-);
-
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
-
-const Playground = (): JSX.Element => {
-  const { form, controller } = useForm<FormValues>({
-    defaultValues: { food: "" },
-    onSubmit: (values) => console.log("onSubmit: ", values),
+export default () => {
+  const [toggle, setToggle] = useState(true);
+  const { form, getState } = useForm({
+    defaultValues: { test: "123" },
   });
-
-  console.log("LOG ===> Re-renders");
+  console.log("LOG ===> ", getState("dirty"));
 
   return (
-    <form ref={form} noValidate>
-      <Optimizer
-        as={Select}
-        name="food"
-        defaultValue=""
-        options={options}
-        parse={(option: any) => option.value}
-        format={(value: any) =>
-          options.find((option: any) => option.value === value)
-        }
-        controller={controller}
-      />
-      <input type="submit" />
+    <form ref={form}>
+      {toggle && <input name="test" />}
+      <button type="button" onClick={() => setToggle(!toggle)}>
+        Toggle
+      </button>
     </form>
   );
 };
-
-export default Playground;
