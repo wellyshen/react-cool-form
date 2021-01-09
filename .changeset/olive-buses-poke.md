414c4897e191f61824370244550604567483cd44
Welly Shen <hivoid19@gmail.com>
2021-01-09 15:03:55 +0800

    Refactor(utils): remove `setTrueValues` utility function

---
+++ .changeset/olive-buses-poke.md
@@ -0,0 +1,5 @@
+---
+"react-cool-form": patch
+---
+
+Refactor(utils): remove `setTrueValues` utility function
--- src/utils/setTrueValues.ts
+++
@@ -1,9 +0,0 @@
-import { Map } from "../types";
-import set from "./set";
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (object: any, names: string[]): Map =>
-  names.reduce((obj, name) => {
-    obj = set(obj, name, true, true);
-    return obj;
-  }, object);
