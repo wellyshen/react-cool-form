0e7def4862b9213ff1fc4312f9fb93ee3eb8c11d
Welly Shen <hivoid19@gmail.com>
2021-01-10 18:07:39 +0800

    Chore(lint): add new rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     ],
     "no-param-reassign": "off",
     "no-underscore-dangle": "off",
+    "no-sparse-arrays": "off",
     "@typescript-eslint/ban-types": "off",
   },
   settings: {
