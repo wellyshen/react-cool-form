f9a7558dd5ed6cc34b41dba828ea75f5749feb53
Welly Shen <hivoid19@gmail.com>
2021-01-10 15:10:53 +0800

    Test(utils): add testing for `arrayToMap`

---
+++ src/utils/__tests__/arrayToMap.ts
@@ -0,0 +1,11 @@
+import arrayToMap from "../arrayToMap";
+
+describe("arrayToMap", () => {
+  it("should work correctly", () => {
+    expect(arrayToMap(["🍎", "🍋", "🥝"])).toEqual({
+      "🍎": true,
+      "🍋": true,
+      "🥝": true,
+    });
+  });
+});
