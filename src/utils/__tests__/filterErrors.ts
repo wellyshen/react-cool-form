eefea0b3884e6424ba60f09d85b59ac1180623e2
Welly Shen <hivoid19@gmail.com>
2021-01-12 14:44:19 +0800

    Test(utils): add testing for `filterErrors`

---
+++ src/utils/__tests__/filterErrors.ts
@@ -0,0 +1,10 @@
+import filterErrors from "../filterErrors";
+
+describe("filterErrors", () => {
+  it("should work correctly", () => {
+    expect(filterErrors({ foo: "🍎" }, { foo: true })).toEqual({ foo: "🍎" });
+    expect(filterErrors({ foo: "🍎" }, {})).toEqual({});
+    const foo = new Date();
+    expect(filterErrors({ foo }, { foo: true })).toEqual({ foo });
+  });
+});
