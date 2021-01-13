6fd7fbb1ab9100a3970122051871915510ab5d3f
Welly <hivoid19@gmail.com>
2021-01-13 23:06:02 +0800

    Chore: update examples

--- examples/custom-field/index.js
+++ examples/custom-field/index.js
@@ -8,7 +8,7 @@ import "./styles.scss";
 function App() {
   const { form, getState, setValue, setTouched } = useForm({
     defaultValues: { username: "" },
-    // ignoreFields: ["username"], // You can also ignore the field by this option
+    // excludeFields: ["username"], // You can also exclude the field by this option
     validate: ({ username }) => {
       const errors = {};
       if (!username.length) errors.username = "Required";
@@ -22,14 +22,14 @@ function App() {
     <form ref={form} noValidate>
       <TextField
         label="Username"
-        name="username" // Used for the "ignoreFields" option
+        name="username" // Used for the "excludeFields" option
         value={value}
         required
         onChange={(e) => setValue("username", e.target.value)} // Update the field's value and set it as touched
         onBlur={() => setTouched("username")} // Set the field as touched for displaying error (if it's not touched)
         error={!!errors.username}
         helperText={errors.username}
-        inputProps={{ "data-rcf-ignore": true }} // Ignore the field via the pre-defined data attribute
+        inputProps={{ "data-rcf-exclude": true }} // Exclude the field via the pre-defined data attribute
       />
       <Button type="submit" variant="contained" color="primary">
         Submit
