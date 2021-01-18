0fa3dde9c40f5efb4504b3bc55185d5a0212e573
Welly Shen <hivoid19@gmail.com>
2021-01-18 21:09:53 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,25 +1,15 @@
-import { useEffect } from "react";
-import { useForm, unset } from "react-cool-form";
-
-enum FieldNames {
-  "test.a.b" = "test.a.b",
-}
+import { useForm } from "react-cool-form";
 
 export default () => {
   const { form } = useForm({
     defaultValues: { test: "test" },
-    onSubmit: (values) => console.log("onSubmit: ", values),
+    onSubmit: (values, { formState }) =>
+      console.log("onSubmit: ", formState.values),
   });
-
-  useEffect(() => {
-    const obj = { a: { b: null } };
-    unset(obj, "a.b");
-    console.log(obj);
-  }, []);
 
   return (
     <form ref={form}>
-      <input name={FieldNames["test.a.b"]} />
+      <input name="test" />
       <input type="submit" />
     </form>
   );
