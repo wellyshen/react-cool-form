9d105b184ebe67929f8c0b881f539b561ec3561c
Welly <hivoid19@gmail.com>
2021-02-15 18:59:57 +0800

    Chore: examples

--- examples/field-level-validation/index.js
+++ examples/field-level-validation/index.js
@@ -26,13 +26,13 @@ const validateUsername = async (value) => {
 };
 
 function App() {
-  const { form, field, getState } = useForm({
+  const { form, field, select } = useForm({
     defaultValues: { username: "", email: "" },
     onSubmit: (values) => alert(JSON.stringify(values, undefined, 2)),
     onError: (errors) => console.log("onError: ", errors)
   });
 
-  console.log("Form is validating: ", getState("isValidating"));
+  console.log("Form is validating: ", select("isValidating"));
 
   return (
     <form ref={form} noValidate>
