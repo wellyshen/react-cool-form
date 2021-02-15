9b3edd18ecc9b2f6ccbf7d30e3859229966cdf68
Welly Shen <hivoid19@gmail.com>
2021-02-16 02:16:05 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -2,14 +2,17 @@ import { useForm } from "react-cool-form";
 
 export default () => {
   const { form } = useForm({
-    defaultValues: { test: [] },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
 
   return (
     <form ref={form}>
-      <input name="test" type="checkbox" value="1" />
-      <input name="test" type="checkbox" value="2" />
+      <input name="foo" type="checkbox" value="1" />
+      <input name="bar" type="radio" value="1" />
+      <select name="baz" multiple>
+        <option>1</option>
+        <option>1</option>
+      </select>
       <input type="submit" />
     </form>
   );
