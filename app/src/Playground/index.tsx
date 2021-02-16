193c738365213d7bda6c1a6f6998d588157cf82b
Welly Shen <hivoid19@gmail.com>
2021-02-16 14:35:41 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -4,13 +4,18 @@ import { useForm } from "react-cool-form";
 export default () => {
   const [value, setValue] = useState("state");
   const { form, controller } = useForm({
-    defaultValues: { foo: "form" },
+    // defaultValues: { foo: "form" },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
 
   return (
     <form ref={form}>
-      <input {...controller("foo", { value, defaultValue: "field" })} />
+      <input
+        {...controller("foo", {
+          value,
+          defaultValue: 0,
+        })}
+      />
       <input type="submit" />
     </form>
   );
