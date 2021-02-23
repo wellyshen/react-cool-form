1d2991aa2490eea48d7ad4c838f11bc72098e298
Welly Shen <hivoid19@gmail.com>
2021-02-23 23:19:51 +0800

    Test: refine warn and invariant testing cases

--- src/utils/invariant.test.ts
+++ src/utils/invariant.test.ts
@@ -4,6 +4,10 @@ describe("invariant", () => {
   it("should work correctly", () => {
     const message = "Oops!";
     expect(() => invariant(true, message)).toThrow(message);
-    expect(() => invariant(false, message)).not.toThrow(message);
+    expect(() => invariant(false, message)).not.toThrow();
+
+    // @ts-expect-error
+    global.__DEV__ = false;
+    expect(() => invariant(true, message)).not.toThrow();
   });
 });
--- src/utils/warn.test.ts
+++ src/utils/warn.test.ts
@@ -1,20 +1,14 @@
 import warn from "./warn";
 
 describe("warn", () => {
-  it("should warn in development", () => {
+  it("should work correctly", () => {
     console.warn = jest.fn();
-    // @ts-expect-error
-    global.__DEV__ = true;
-
     warn("🍎", "🍋");
     expect(console.warn).toHaveBeenCalledWith("🍎", "🍋");
-  });
 
-  it("should not warn in production", () => {
     console.warn = jest.fn();
     // @ts-expect-error
     global.__DEV__ = false;
-
     warn("🍎", "🍋");
     expect(console.warn).not.toHaveBeenCalled();
   });
