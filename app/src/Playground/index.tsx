bc4159b566f90f0d2783aea012cd732caafac767
Welly Shen <hivoid19@gmail.com>
2021-02-26 20:26:33 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,35 +1,44 @@
 /* eslint-disable no-console */
 
 import { useState } from "react";
-import { useForm } from "react-cool-form";
+import { useForm, useControlled } from "react-cool-form";
 
 interface FormValues {
   foo: string;
 }
 
+const Field = ({ name, ...rest }: any) => {
+  const [props, meta] = useControlled(name, { ...rest });
+
+  return <input {...props} />;
+};
+
 export default () => {
   const [show, setShow] = useState(true);
   const { form, select, reset } = useForm<FormValues>({
+    id: "form-1",
     // defaultValues: { foo: "form test" },
-    // shouldRemoveField: false,
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
 
-  console.log("LOG ===> Form re-renders: ", select("values"));
+  console.log(
+    "LOG ===> Form re-renders: ",
+    // @ts-expect-error
+    select("values.bar", { defaultValues: { bar: "field test" } })
+  );
 
   return (
-    <>
+    <form ref={form} noValidate>
+      {/* {show && <input name="foo" defaultValue="field test" />} */}
+      {show && <Field name="bar" formId="form-1" defaultValue="field test" />}
+      <input type="submit" />
+      <input type="reset" />
       <button type="button" onClick={() => reset()}>
         Reset
       </button>
       <button type="button" onClick={() => setShow(!show)}>
         Toggle
       </button>
-      <form ref={form} noValidate>
-        {show && <input name="foo" defaultValue="field test" />}
-        <input type="submit" />
-        <input type="reset" />
-      </form>
-    </>
+    </form>
   );
 };
