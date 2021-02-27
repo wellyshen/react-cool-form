655d25640304cd6066e377beaf784ccb6ba9b2ae
Welly Shen <hivoid19@gmail.com>
2021-02-27 14:14:36 +0800

    Chore: adjust exported modules ordering

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as useForm } from "./useForm";
-export { default as useFormState } from "./useFormState";
 export { default as useFormMethods } from "./useFormMethods";
+export { default as useFormState } from "./useFormState";
 export { default as useControlled } from "./useControlled";
 export { get, set, unset } from "./utils";
