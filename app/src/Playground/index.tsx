ed1c9f50e34282dcad10b95a5b1ffbf330030966
Welly Shen <hivoid19@gmail.com>
2021-03-07 00:58:44 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,17 +1,31 @@
 /* eslint-disable no-console */
 
-import { useForm, useFormState } from "react-cool-form";
+import { useState } from "react";
+import { useForm } from "react-cool-form";
 
 export default () => {
+  const [t0, setT0] = useState(true);
+  const [t1, setT1] = useState(true);
   const { form } = useForm({
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
-  console.log("LOG ===> ", useFormState("values.foo"));
 
   return (
     <form ref={form}>
-      <input name="foo" type="checkbox" />
+      {/* <select name="foo">
+        {t0 && <option value="v0">v0</option>}
+        <option value="v1">v1</option>
+      </select> */}
+      {t0 && <input name="foo" type="radio" value="R1" />}
+      {t1 && <input name="foo" type="radio" value="R2" />}
+      <br />
       <input type="submit" />
+      <button type="button" onClick={() => setT0(!t0)}>
+        T0
+      </button>
+      <button type="button" onClick={() => setT1(!t1)}>
+        T1
+      </button>
     </form>
   );
 };
