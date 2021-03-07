d848d50aa973cc6a40e5f02f62e08d07f25462a7
Welly Shen <hivoid19@gmail.com>
2021-03-08 00:32:44 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -7,20 +7,20 @@ export default () => {
   const [t0, setT0] = useState(true);
   const [t1, setT1] = useState(true);
   const { form, select } = useForm({
-    defaultValues: { bar: "test", foo: ["v1"] },
+    defaultValues: { bar: "test", foo: "v1" },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
   console.log("LOG ===> ", select("values.foo"));
 
   return (
     <form ref={form}>
-      {/* <select name="foo">
+      <select name="foo">
         {t0 && <option value="v0">v0</option>}
-        <option value="v1">v1</option>
-      </select> */}
-      {t0 && <input name="bar" />}
+        {t1 && <option value="v1">v1</option>}
+      </select>
+      {/* {t0 && <input name="bar" />}
       {t0 && <input name="foo" type="checkbox" value="v1" />}
-      {t1 && <input name="foo" type="checkbox" value="v2" />}
+      {t1 && <input name="foo" type="checkbox" value="v2" />} */}
       <br />
       <input type="submit" />
       <input type="reset" />
