597137ed7342fa1045f481b38bc0ca08ca937d9b
Welly Shen <hivoid19@gmail.com>
2021-03-11 13:02:39 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,17 +1,16 @@
 /* eslint-disable no-console */
 
 import { useState, useEffect } from "react";
-import { useForm } from "react-cool-form";
+import { useForm, useControlled } from "react-cool-form";
 
 export default () => {
   const [t0, setT0] = useState(true);
-  const [t1, setT1] = useState(true);
   const { form, select, setValue } = useForm({
     // defaultValues: { foo: "test" },
     shouldRemoveField: false,
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
-  // console.log("LOG ===> ", select("values.foo"));
+  const [fieldProps] = useControlled("foo", { defaultValue: "test" });
 
   useEffect(() => {
     // setValue("foo", "new test");
@@ -19,13 +18,11 @@ export default () => {
 
   return (
     <form ref={form}>
-      {t0 && <input name="foo" defaultValue="test" />}
+      {t0 && <input {...fieldProps} />}
       <input type="submit" />
-      <button type="button" onClick={() => setT0(!t0)}>
+      <input type="reset" />
+      {/* <button type="button" onClick={() => setT0(!t0)}>
         T0
-      </button>
-      {/* <button type="button" onClick={() => setT1(!t1)}>
-        T1
       </button> */}
     </form>
   );
