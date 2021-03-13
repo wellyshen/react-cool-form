27680cbe35ae42ef776a9d2d2e96991054cfabab
Welly Shen <hivoid19@gmail.com>
2021-03-13 16:45:53 +0800

    Chiore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,29 +1,17 @@
 /* eslint-disable no-console */
 
-import { useState, useEffect } from "react";
-import { useForm, useControlled } from "react-cool-form";
+import { useForm } from "react-cool-form";
 
 export default () => {
-  const [t0, setT0] = useState(true);
-  const { form, select, setValue } = useForm({
-    // defaultValues: { foo: "test" },
-    shouldRemoveField: false,
+  const { form } = useForm({
+    defaultValues: { foo: "test" },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
-  const [fieldProps] = useControlled("foo", { defaultValue: "test" });
-
-  useEffect(() => {
-    // setValue("foo", "new test");
-  }, [setValue]);
 
   return (
     <form ref={form}>
-      {t0 && <input {...fieldProps} />}
+      <input name="foo" />
       <input type="submit" />
-      <input type="reset" />
-      {/* <button type="button" onClick={() => setT0(!t0)}>
-        T0
-      </button> */}
     </form>
   );
 };
