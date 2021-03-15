3de9c66adbad97f4aa147e71462d2156240f5916
Welly Shen <hivoid19@gmail.com>
2021-03-15 21:03:48 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -3,27 +3,31 @@
 import { useState, useEffect } from "react";
 import { useForm, useControlled } from "react-cool-form";
 
+const Field = ({ name, ...rest }: any) => {
+  const [props] = useControlled(name, rest);
+  return <input {...props} />;
+};
+
 export default () => {
   const [t0, setT0] = useState(true);
-  const { form, select, setValue } = useForm({
-    // defaultValues: { foo: "test" },
+  const { form, reset } = useForm({
+    // defaultValues: { foo: "form test" },
     shouldRemoveField: false,
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
-  const [fieldProps] = useControlled("foo", { defaultValue: "test" });
 
   useEffect(() => {
-    // setValue("foo", "new test");
-  }, [setValue]);
+    // reset({ foo: "form test" });
+  }, [reset]);
 
   return (
     <form ref={form}>
-      {t0 && <input {...fieldProps} />}
+      {t0 && <Field name="foo" defaultValue="field test" />}
       <input type="submit" />
       <input type="reset" />
-      {/* <button type="button" onClick={() => setT0(!t0)}>
+      <button type="button" onClick={() => setT0(!t0)}>
         T0
-      </button> */}
+      </button>
     </form>
   );
 };
