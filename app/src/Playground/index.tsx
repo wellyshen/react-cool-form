ed0a15094d6944a89ac085b30aef9b15a98685f1
Welly Shen <hivoid19@gmail.com>
2021-03-23 01:09:53 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,18 +1,29 @@
 /* eslint-disable no-console */
 
-import { useForm } from "react-cool-form";
+import { useForm, useFieldArray, useControlled } from "react-cool-form";
+
+const Field = ({ name, ...restProps }: any) => {
+  const [props] = useControlled(name, restProps);
+  return <input {...props} />;
+};
 
 export default () => {
   const { form, mon } = useForm({
-    defaultValues: { foo: "test" },
+    defaultValues: { foo: [{ name: "test-1" }] },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
+  const [fields, { push }] = useFieldArray("foo");
 
   console.log("LOG ===> Re-rendering", mon({ foo: "foo" }));
 
   return (
     <form ref={form}>
-      <input name="foo" />
+      {fields.map(([fieldName]) => (
+        <Field key={fieldName} name={`${fieldName}.name`} />
+      ))}
+      <button type="button" onClick={() => push({ name: "test-2" })}>
+        Push
+      </button>
       <input type="submit" />
     </form>
   );
