e0aaafb6e84676858fcf50b0f837af737c136afe
Welly Shen <hivoid19@gmail.com>
2021-03-26 15:59:13 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -7,13 +7,18 @@ export default () => {
     // defaultValues: { foo: [{ name: "test-1" }] },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
-  const [fields] = useFieldArray("foo", { defaultValue: [{ name: "test-1" }] });
+  const [fields, { push }] = useFieldArray("foo", {
+    defaultValue: [{ name: "test-1" }],
+  });
 
   return (
     <form ref={form}>
       {fields.map((fieldName) => (
         <input key={fieldName} name={`${fieldName}.name`} />
       ))}
+      <button type="button" onClick={() => push({ name: "test-2" })}>
+        Push
+      </button>
       <input type="submit" />
     </form>
   );
