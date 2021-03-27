876a6f444465d0383685544bda903ae242f7af68
Welly Shen <hivoid19@gmail.com>
2021-03-28 02:04:18 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,21 +1,58 @@
 /* eslint-disable no-console */
 
 import { useState } from "react";
-import { useForm } from "react-cool-form";
+import { useForm, useControlled } from "react-cool-form";
+
+const Field = ({ name, ...rest }: any) => {
+  const [props] = useControlled(name, rest);
+  return <input {...props} />;
+};
 
 export default () => {
   const [show, setShow] = useState(true);
-  const { form } = useForm({
-    defaultValues: { foo: "test" },
+  const { form, field, setFocus, setTouched } = useForm({
+    defaultValues: { foo: "", baz: "", bar: "", aa: false },
+    focusOnError: false,
     onSubmit: (values) => console.log("onSubmit: ", values),
+    onError: (errors) => console.log("onError: ", errors),
   });
 
   return (
     <form ref={form}>
-      {show && <input name="foo" />}
+      {/* {show && (
+        <input
+          name="foo"
+          // ref={field((value) => (!value.length ? "Required" : false))}
+        />
+      )} */}
+      {/* <input
+        type="checkbox"
+        name="aa"
+        ref={field((value) => (!value ? "Required" : false))}
+      /> */}
+      {show && (
+        <Field
+          name="baz"
+          validate={(value: any) => (!value.length ? "Required" : false)}
+        />
+      )}
+      <input
+        name="bar"
+        defaultValue="field-test"
+        ref={field((value) => (!value.length ? "Required" : false))}
+      />
       <input type="submit" />
       <button type="button" onClick={() => setShow(!show)}>
         Toggle
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setFocus("baz");
+          setTimeout(() => setTouched("baz"), 1000);
+        }}
+      >
+        Set Focus
       </button>
     </form>
   );
