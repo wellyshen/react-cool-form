f4d2a96f87f419bd6521b9abdda48add7e3f50fd
Welly Shen <hivoid19@gmail.com>
2021-03-29 23:07:36 +0800

    Chore: remove `isString` utils

--- src/utils/stringToPath.ts
+++ src/utils/stringToPath.ts
@@ -1,8 +1,7 @@
 import compact from "./compact";
-import isString from "./isString";
 
 export default (str: string): string[] => {
-  if (!isString(str)) throw new TypeError("Expected a string.");
+  if (typeof str !== "string") throw new TypeError("Expected a string.");
   if (!str.length) return [];
 
   return compact(str.split(/[.[\]]+/));
