f299a24678d8ae43daa6a07e59c40e25f07b06b0
Welly Shen <hivoid19@gmail.com>
2021-04-02 00:41:51 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -3,33 +3,15 @@
 import { useForm } from "react-cool-form";
 
 export default () => {
-  const { form, runValidation, mon, field } = useForm({
+  const { form } = useForm({
     defaultValues: { foo: "" },
-    // validate: async () => {
-    //   // eslint-disable-next-line compat/compat
-    //   await new Promise((resolve) => setTimeout(resolve, 1000));
-    //   return { foo: "Required" };
-    // },
     onSubmit: (values) => console.log("onSubmit: ", values),
     onError: (errors) => console.log("onError: ", errors),
   });
 
-  console.log("LOG ===> ", mon("errors"));
-
   return (
     <form ref={form} noValidate>
-      <input
-        name="foo"
-        ref={field(async () => {
-          // eslint-disable-next-line compat/compat
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-          return false;
-          // return "Required";
-        })}
-      />
-      <button type="button" onClick={() => runValidation(null, true)}>
-        Validate
-      </button>
+      <input data-rcf-exclude />
     </form>
   );
 };
