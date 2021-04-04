af2f19de385e3afdc567f1de5b672b3d8bbccdbd
Welly <hivoid19@gmail.com>
2021-04-04 16:05:39 +0800

    Chore: update example

--- examples/yup/index.js
+++ examples/yup/index.js
@@ -1,16 +1,12 @@
 import { render } from "react-dom";
 import { useForm, set } from "react-cool-form";
 import * as yup from "yup";
+import Joi from "joi";
 
 import "./styles.scss";
 
-const schema = yup.object().shape({
-  username: yup.string().required(),
-  email: yup.string().email().required(),
-  password: yup.string().required().min(6)
-});
-
-const validate = async (values) => {
+// Reusable validation function for Yup
+const validateWithYup = (schema) => async (values) => {
   let errors = {};
 
   try {
@@ -22,10 +18,37 @@ const validate = async (values) => {
   return errors;
 };
 
+// Reusable validation function for Joi
+const validateWithJoi = (schema) => (values) => {
+  let errors = {};
+
+  const { error: joiError } = schema.validate(values, { abortEarly: false });
+
+  if (joiError)
+    joiError.details.forEach(({ path, message }) =>
+      set(errors, path[0], message)
+    );
+
+  return errors;
+};
+
+const yupSchema = yup.object().shape({
+  username: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required().min(6)
+});
+
+const JoiSchema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().email({ tlds: false }).required(),
+  password: Joi.string().required().min(6)
+});
+
 function App() {
   const { form } = useForm({
     defaultValues: { username: "", email: "", password: "" },
-    validate,
+    validate: validateWithYup(yupSchema),
+    // validate: validateWithJoi(JoiSchema),
     onSubmit: (values) => alert(JSON.stringify(values, undefined, 2)),
     onError: (errors) => console.log("onError: ", errors)
   });
