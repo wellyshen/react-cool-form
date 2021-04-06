00e6648939f40f0f180efef7a3c88d06b462a738
Welly Shen <hivoid19@gmail.com>
2021-04-06 13:50:29 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,25 +1,36 @@
 /* eslint-disable no-console */
 
 import { useState } from "react";
-import { useForm } from "react-cool-form";
+import { useForm, useControlled } from "react-cool-form";
+
+const Field = ({ name, ...rest }: any) => {
+  const [props] = useControlled(name, rest);
+  return <input {...props} />;
+};
 
 export default () => {
   const [show, setShow] = useState(true);
   const { form, reset } = useForm({
-    defaultValues: { foo: "form test" },
+    defaultValues: { foo: "form test", bar: "form test" },
     onSubmit: (values) => console.log("onSubmit: ", values),
   });
 
   return (
     <form ref={form}>
-      {show && <input name="foo" />}
+      {show && <input name="foo" defaultValue="field test" />}
+      {show && <Field name="bar" defaultValue="field test" />}
       <input type="submit" />
       <button type="button" onClick={() => setShow(!show)}>
         Toggle
       </button>
       <button
         type="button"
-        onClick={() => console.log("LOG ===> ", reset({ foo: "reset test" }))}
+        onClick={() =>
+          console.log(
+            "LOG ===> ",
+            reset({ foo: "reset test", bar: "reset test" })
+          )
+        }
       >
         Reset
       </button>
