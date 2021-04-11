43cfbddfff091d2375387a93a18312fc6c046244
Welly Shen <hivoid19@gmail.com>
2021-04-11 18:24:32 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,26 +1,44 @@
 /* eslint-disable no-console */
 
 import { useState } from "react";
-import { useForm, useControlled } from "react-cool-form";
+import { useForm, useFieldArray, useControlled } from "react-cool-form";
 
 const Field = ({ name, ...rest }: any) => {
   const [props] = useControlled(name, rest);
   return <input {...props} />;
 };
 
+const FieldArray = ({ name, ...rest }: any) => {
+  const [fields] = useFieldArray(name, rest);
+  return (
+    <>
+      {fields.map((fieldName) => (
+        <input key={fieldName} name={`${fieldName}.a`} />
+      ))}
+    </>
+  );
+};
+
 export default () => {
   const [show, setShow] = useState(true);
-  const { form, getState } = useForm({
-    shouldRemoveField: false,
+  const { form, getState, setValue } = useForm({
     defaultValues: {
-      foo: "test",
+      // foo: [{ a: "form test", b: "form test" }],
     },
     onSubmit: (values) => console.log("onSubmit: ", values),
+  });
+  const [fields] = useFieldArray("foo", {
+    defaultValue: [{ a: "field test", b: "field test" }],
   });
 
   return (
     <form ref={form}>
-      {show && <Field name="foo" />}
+      {fields.map((name) => (
+        <div key={name}>
+          {show && <input name={`${name}.a`} />}
+          {show && <Field name={`${name}.b`} />}
+        </div>
+      ))}
       <button type="button" onClick={() => setShow(!show)}>
         Toggle
       </button>
@@ -29,6 +47,9 @@ export default () => {
         onClick={() => console.log("LOG ===> ", getState())}
       >
         Get State
+      </button>
+      <button type="button" onClick={() => setValue("foo[0].b", undefined)}>
+        Set Value
       </button>
       <input type="submit" />
     </form>
