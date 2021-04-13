1b8221b45a9dad9be37e8719f837ba4ffe2c001c
Welly Shen <hivoid19@gmail.com>
2021-04-13 22:59:20 +0800

    Chore(playground): test something...

--- app/src/Playground/index.tsx
+++ app/src/Playground/index.tsx
@@ -1,63 +1,29 @@
 /* eslint-disable no-console */
 
 import { useState } from "react";
-import { useForm, useFieldArray, useControlled } from "react-cool-form";
-
-const Field = ({ name, ...rest }: any) => {
-  const [props] = useControlled(name, rest);
-  return <input {...props} />;
-};
-
-const FieldArray = ({ name, ...rest }: any) => {
-  const [fields] = useFieldArray(name, rest);
-  return (
-    <>
-      {fields.map((fieldName) => (
-        <input key={fieldName} name={`${fieldName}.a`} />
-      ))}
-    </>
-  );
-};
+import { useForm } from "react-cool-form";
 
 export default () => {
-  const [show, setShow] = useState(false);
-  const { form, getState, setValue } = useForm({
+  const [show1, setShow1] = useState(true);
+  const [show2, setShow2] = useState(true);
+  const { form } = useForm({
     defaultValues: {
-      // foo: [],
+      foo: [{ a: "t1" }, { a: "t2" }],
     },
     onSubmit: (values) => console.log("onSubmit: ", values),
-  });
-  const [fields, { push }] = useFieldArray("foo", {
-    // defaultValue: [{ a: "field test", b: "field test" }],
   });
 
   return (
     <form ref={form}>
-      {fields.map((name) => (
-        <div key={name}>
-          {show && <input name={`${name}.a`} defaultValue="field test" />}
-          {/* <input name={`${name}.a`} /> */}
-          {show && <Field name={`${name}.b`} defaultValue="field test" />}
-          {/* <Field name={`${name}.b`} /> */}
-        </div>
-      ))}
-      <button type="button" onClick={() => setShow(!show)}>
+      {show1 && <input name="foo[0].a" />}
+      {show2 && <input name="foo[1].a" />}
+      <button type="button" onClick={() => setShow1(!show1)}>
         Toggle
       </button>
-      <button type="button" onClick={() => push({ a: "form test" })}>
-        Push
-      </button>
-      <button
-        type="button"
-        onClick={() => console.log("LOG ===> ", getState())}
-      >
-        Get State
-      </button>
-      <button type="button" onClick={() => setValue("foo[0].b", undefined)}>
-        Set Value
+      <button type="button" onClick={() => setShow2(!show2)}>
+        Toggle
       </button>
       <input type="submit" />
-      <input type="reset" />
     </form>
   );
 };
