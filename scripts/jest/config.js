5c79092c20ddd685a8e54695088a7148bf69f5d0
Welly Shen <hivoid19@gmail.com>
2021-06-26 00:44:07 +0800

    chore(jest): use `jsdom` environment

--- scripts/jest/config.js
+++ scripts/jest/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   rootDir: "../../",
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/scripts/jest/setup.ts"],
   collectCoverageFrom: ["src/**/*.ts", "!**/index.ts", "!src/types/*"],
   globals: { __DEV__: true },
